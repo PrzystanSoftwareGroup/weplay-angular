e42120f6b0d6fa3f4ca366a6ff8912502d9a010c
luckybard <bartosz.mucha@outlook.com>
2023-06-17 22:03:02 +0200

    feat: generate calendar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {ClarityModule} from "@clr/angular";
-import { HeaderComponent } from './component/header/header.component';
-
+import {HeaderComponent} from './component/header/header.component';
 import '@cds/core/icon/register.js';
 import {ClarityIcons, cogIcon, playIcon} from '@cds/core/icon';
+import {CalendarComponent} from './component/calendar/calendar.component';
 
-ClarityIcons.addIcons(cogIcon,playIcon);
+
+ClarityIcons.addIcons(cogIcon, playIcon);
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule, BrowserAnimationsModule, ClarityModule
