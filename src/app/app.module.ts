77b55680c75dec558216a0359a41aa2bcee8fc0c
luckybard <bartosz.mucha@outlook.com>
2023-06-17 21:27:12 +0200

    feat: add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {ClarityModule} from "@clr/angular";
+import { HeaderComponent } from './component/header/header.component';
+
+import '@cds/core/icon/register.js';
+import {ClarityIcons, cogIcon, playIcon} from '@cds/core/icon';
+
+ClarityIcons.addIcons(cogIcon,playIcon);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule, BrowserAnimationsModule, ClarityModule
